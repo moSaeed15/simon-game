a1c4a999f9990ee8ffa6d9d056d615418d581994
moh151 <67608414+moh151@users.noreply.github.com>
2020-08-02 21:01:29 +0200

    Update game.js

--- game.js
+++ game.js
@@ -6,13 +6,6 @@ var userClickedPattern = [];
 
 var started = false;
 var level = 0;
-$(document).click(function() {
-  if (!started) {
-    $("#level-title").text("Level " + level);
-    nextSequence();
-    started = true;
-  }
-});
 $(document).keypress(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
