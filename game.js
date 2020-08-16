ed73c493f53e7b4196fc1a356032ce1952b0a579
moh151 <67608414+moh151@users.noreply.github.com>
2020-08-16 18:11:37 +0200

    Update game.js

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ var userClickedPattern = [];
 
 var started = false;
 var level = 0;
-$(document).dblclick(function() {
+$(document).keyup(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
